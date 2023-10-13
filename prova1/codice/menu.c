bcc6a7dc7e487a952ba3a9da7cd2b637cf8af249
mgcarofano <m.carofano@studenti.unina.it>
2023-10-13 13:45:30 +0200

    Divisi menu e strategie
    + il menu crea dinamicamente il file .pbs
    + nel menu è ora possibile inserire anche la quantità di numeri da sommare
    + modificato algoritmo di controllo dell'input per recuperare valori interi

--- prova1/codice/menu.c
+++ prova1/codice/menu.c
@@ -4,12 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <math.h>
 
 /* ************************************************************************** */
+// ENUMERAZIONI E COSTANTI
 
+#define NOME_PROVA "prova1"
+#define NODE_NUMBER "8"
+
+/* ************************************************************************** */
 // DEFINIZIONE DI FUNZIONI E TIPI ACCESSORI
 
-int scegliOperazione(int lim_sup) {
+int getIntegerFromInput() {
 	int scelta_intero = 0, lim_inf = 1;
 	
 	char *buffer = NULL;
@@ -25,34 +31,55 @@ int scegliOperazione(int lim_sup) {
 	chars_read = getline(&buffer, &bufsize, stdin); // inizializzazione del buffer con caratteri estratti dallo stream di input
 	
 	if (chars_read < 0) {
+
 		printf("Errore nella lettura dell'input!");
 		free(buffer);
 		exit(1);
+
 	} else {
+
 		int digit_val = 0;
-		for (int i = 0; i < chars_read; i++) {
+		int i = 0;
+		int exp = 0;
+
+		// printf("--- chars_read: %zd ---\n\n", chars_read);
+
+		for (i = chars_read-2; i >= 0; i--) {
+			// printf("--- buffer[i]: %c ---\n", buffer[i]);
+			// printf("--- exp: %d ---\n", exp);
+			// printf("--- pow(10, exp): %f ---\n", pow(10, exp));
+
 			if (isdigit(buffer[i])) {
-				digit_val = 10 * digit_val + (buffer[i] - '0'); // per ottenere il valore intero del carattere ASCII
+				// printf("--- digit_val: %d ---\n", digit_val);
+				digit_val = pow(10, exp) * (digit_val + (buffer[i] - '0')); // per ottenere il valore intero del carattere ASCII
 				scelta_intero = scelta_intero + digit_val;
+				// printf("--- scelta_intero: %d ---\n", digit_val);
+				// printf("--- digit_val: %d ---\n\n", digit_val);
 			} else {
 				scelta_intero = 0;
-				break;
+				printf("Puoi inserire solo valori numerici!\n");
+				printf("Applicazione terminata.\n");
+				free(buffer);
+				exit(1);
 			}
+
+			digit_val = 0;
+			exp++;
 		}
 	}
 
-	printf("scelta_intero: %d\n", scelta_intero);
-	// se scelta_intero == 0, allora il suo valore non è stato calcolato dal controllo di isdigit(buffer[i])
-	if(!(scelta_intero >= lim_inf && scelta_intero <= lim_sup)) {
+	free(buffer);
+	return scelta_intero;
+}
+
+void checkScelta(int scelta, int lim_inf, int lim_sup) {
+	if(!(scelta >= lim_inf && scelta <= lim_sup)) {
 		printf("Puoi inserire solo un valore numerico intero compreso tra %d e %d!\n", lim_inf, lim_sup);
 		printf("Applicazione terminata.\n");
-		free(buffer);
 		exit(1);
 	}
 
-	printf("\n");
-	free(buffer);
-	return scelta_intero;
+	return;
 }
 
 /* ************************************************************************** */
@@ -62,10 +89,10 @@ int main(int argc, char const *argv[]) {
 	/* ************************************************************************ */
 	// DEFINIZIONE DELLE VARIABILI
 
-	int scelta = 0, cont = 0;
+	int scelta = 0, cont = 0, q_num = 0;
+	FILE *pbs_file;
 
 	/* ************************************************************************ */
-
 	// INTRODUZIONE
 
 	printf("\n");
@@ -80,44 +107,67 @@ int main(int argc, char const *argv[]) {
 	printf("%d. \t Applicazione della strategia 3.\n", ++cont);
 	printf("%d. \t Esecuzione dell'esempio d'uso (somma di 1).\n", ++cont);
 	printf("%d. \t Chiudere l'applicazione.\n", ++cont);
-
-  	scelta = scegliOperazione(cont);
+  	scelta = getIntegerFromInput();
+	checkScelta(scelta, 1, cont);
 
 	/* ************************************************************************ */
 
-  	switch (scelta) {
-		case 1: // Applicazione della strategia 1.
-		{
-			// ...
-			break;
+	if (scelta != 5) {
+
+		printf("Inserisci la quantita' di numeri da sommare: \n");
+		q_num = getIntegerFromInput();
+
+		// printf("q_num: %d\n", q_num);
+
+		if (q_num < 0) {
+			printf("Puoi inserire solo un valore numerico intero maggiore o uguale a 0!\n");
+			printf("Applicazione terminata.\n");
+			exit(1);
 		}
-		case 2: // Applicazione della strategia 2.
-		{
-			// ...
-			break;
-		}
-		case 3: // Applicazione della strategia 3.
-		{
-			// ...
-			break;
-		}
-		case 4: // Esecuzione dell'esempio d'uso (somma di 1).
-		{
-			// ...
-			break;
-		}
-		case 5: // Chiudere l'applicazione
-		{
-			break;
-		}
-		default:
-		{
-			printf("Comando non riconosciuto!");
-			break;
-		}
+
+		/* ******************************************************************** */
+		// CREAZIONE DEL FILE DI ESECUZIONE .PBS
+
+		pbs_file = fopen(NOME_PROVA".pbs", "w");
+
+		fprintf(pbs_file,
+					"#!/bin/bash\n"
+					"\n"
+					"#PBS -q studenti\n"
+					"#PBS -l nodes=" NODE_NUMBER "\n"
+					"#PBS -N " NOME_PROVA "\n"
+					"#PBS -o " NOME_PROVA ".out\n"
+					"#PBS -e " NOME_PROVA ".err\n"
+					"\n"
+					"echo --- \n"
+					"PBS_NODENUMBER=" NODE_NUMBER "\n"
+					"PBS_O_WORKDIR=$PBS_O_HOME/" NOME_PROVA "/codice\n"
+					"\n"
+					"echo Compilazione in esecuzione\n"
+					"/usr/lib64/openmpi/1.4-gcc/bin/mpicc"
+					"-o $PBS_O_WORKDIR/" NOME_PROVA " $PBS_O_WORKDIR/" NOME_PROVA ".c\n"
+					"\n"
+					"echo Job in esecuzione\n"
+					"/usr/lib64/openmpi/1.4-gcc/bin/mpiexec"
+					"-machinefile $PBS_NODEFILE -n $PBS_NODENUMBER $PBS_O_WORKDIR/" NOME_PROVA " %d %d", scelta, q_num
+		);
+
+		fclose(pbs_file);
+
+		/* ******************************************************************** */
+		// ESECUZIONE DEL COMANDO QSUB
+
+
+
+		/* ******************************************************************** */
+		// STAMPA DELL'OUTPUT
+
+
+
 	}
 
-  	/* ************************************************************************ */
+	/* ************************************************************************ */
+	// USCITA DALL'APPLICATIVO
 
 	printf("Applicazione terminata.\n");
 	return 0;
@@ -130,5 +180,8 @@ int main(int argc, char const *argv[]) {
 https://stackoverflow.com/questions/35890237/how-to-create-a-type-definition-for-a-string-in-c
 https://stackoverflow.com/questions/5029840/convert-char-to-int-in-c-and-c
 https://opensource.com/article/22/5/safely-read-user-input-getline
+https://stackoverflow.com/questions/19209141/how-do-i-execute-a-shell-built-in-command-with-a-c-function
+https://www.w3schools.com/c/c_files_write.php
+https://stackoverflow.com/questions/5256313/c-c-macro-string-concatenation
 
 */
---
+++ prova1/codice/prova1.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+// LIBRERIE
+
+// #include <mpi.h>
+#include <stdio.h>
+
+/* ************************************************************************** */
+// ENUMERAZIONI E COSTANTI
+
+#define NOME_PROVA "prova1"
+
+/* ************************************************************************** */
+// DEFINIZIONE DI FUNZIONI E TIPI ACCESSORI
+
+
+
+/* ************************************************************************** */
+
+int main(int argc, char const *argv[]) {
+
+	/* ************************************************************************ */
+	// DEFINIZIONE DELLE VARIABILI
+
+	int scelta = argv[1];
+	int q_num = argv[2];
+
+	/* ************************************************************************ */
+
+	// INTRODUZIONE
+
+	printf("Inizio esecuzione.\n\n");
+
+	/* ************************************************************************ */
+
+  	switch (scelta) {
+		case 1: // Applicazione della strategia 1.
+		{
+			printf("Applicazione della strategia 1.\n");
+			break;
+		}
+		case 2: // Applicazione della strategia 2.
+		{
+			printf("Applicazione della strategia 2.\n");
+			break;
+		}
+		case 3: // Applicazione della strategia 3.
+		{
+			printf("Applicazione della strategia 3.\n");
+			break;
+		}
+		case 4: // Esecuzione dell'esempio d'uso (somma di 1).
+		{
+			printf("Esecuzione dell'esempio d'uso (somma di 1).\n");
+			break;
+		}
+		default:
+		{
+			printf("Comando non riconosciuto!\n");
+			break;
+		}
+	}
+
+  	/* ************************************************************************ */
+
+	printf("%d", q_num);
+	printf("\nEsecuzione terminata.\n");
+	return 0;
+}
+
+/* ************************************************************************** */
+
+/* RIFERIMENTI
+
+https://stackoverflow.com/questions/35890237/how-to-create-a-type-definition-for-a-string-in-c
+https://stackoverflow.com/questions/5029840/convert-char-to-int-in-c-and-c
+https://opensource.com/article/22/5/safely-read-user-input-getline
+https://stackoverflow.com/questions/19209141/how-do-i-execute-a-shell-built-in-command-with-a-c-function
+https://www.w3schools.com/c/c_files_write.php
+https://stackoverflow.com/questions/5256313/c-c-macro-string-concatenation
+
+*/
