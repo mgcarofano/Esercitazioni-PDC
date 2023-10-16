c03c8880d4e9dec922ae341f5eedcb7bdf59f2c6
mgcarofano <m.carofano@studenti.unina.it>
2023-10-16 16:29:59 +0200

    Completata lettura e distribuzione dei dati + strategia 1
    + modificato const char *argv[] in char **argv per motivi di compatibilità
    + aggiunto MPI_Finalize();
    + migliorata documentazione interna
    + testato calcolo dei tempi: OK
    + testato strategia 1: OK

--- prova1/codice/prova1.c
+++ prova1/codice/prova1.c
@@ -8,6 +8,7 @@
 #include <limits.h>
 #include <string.h>
 #include <math.h>
+#include <tgmath.h>
 
 /* **************************************************************************** */
 // ENUMERAZIONI E COSTANTI
@@ -17,7 +18,7 @@
 /* **************************************************************************** */
 // DEFINIZIONE DI FUNZIONI E TIPI ACCESSORI
 
-int argToInt(const char *arg) {
+int argToInt(char *arg) {
     
 	char *p;
 	errno = 0; // Si inizializza il valore di 'errno' nella libreria <errno.h>
@@ -26,39 +27,49 @@ int argToInt(const char *arg) {
 	if (strlen(arg) == 0) {
 		printf("Errore nella lettura degli argomenti di input!\n\n");
 		printf("Esecuzione terminata.\n");
+		MPI_Finalize();
 		exit(1);
     }
 
 	if (*p != '\0' || errno != 0) {
 		printf("Errore nella lettura degli argomenti di input!\n\n");
 		printf("Esecuzione terminata.\n");
+		MPI_Finalize();
 		exit(1);
     }
+
+	/*
+		Per una corretta conversione da long a int, si controlla
+		che il valore long sia rappresentabile come intero.
+	*/
     
-    if (out_long < INT_MIN || out_long > INT_MAX) { // Per una corretta conversione da long a int, si controlla che il valore long sia rappresentabile come intero
+    if (out_long < INT_MIN || out_long > INT_MAX) {
         printf("Errore nella lettura degli argomenti di input!\n\n");
 		printf("Esecuzione terminata.\n");
+		MPI_Finalize();
 		exit(1);
     }
 	
     return (int)out_long;
 }
 
-double argToDouble(const char *arg) {
+double argToDouble(char *arg) {
     
 	char *p;
 	errno = 0;
-    double out_double = strtod(arg, &p);
+	double out_double = strtod(arg, &p);
 
 	if (strlen(arg) == 0) {
 		printf("Errore nella lettura degli argomenti di input!\n\n");
 		printf("Esecuzione terminata.\n");
+		MPI_Finalize();
 		exit(1);
     }
 
 	if (*p != '\0' || errno != 0) {
 		printf("Errore nella lettura degli argomenti di input!\n\n");
 		printf("Esecuzione terminata.\n");
+		MPI_Finalize();
 		exit(1);
     }
 	
@@ -67,45 +78,68 @@ double argToDouble(const char *arg) {
 
 /* **************************************************************************** */
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char **argv) {
 
 	/* ************************************************************************ */
 	// DEFINIZIONE DELLE VARIABILI
 
-	int menum = 0, nproc = 0;
-	int n = 0, nloc = 0, tag = 0, rest=0;
-	int tmp = 0, start = 0;
-	int *x, *xloc;
+	int scelta = 0, q_num = 0, time_calc = 0;
+	
+	int id_proc = 0, n_proc = 0, rest = 0;
+	int q_loc = 0, tag = 1;
+	int tmp = 0, id_op = 0;
+	int i = 0, p = 0;
 
-	// PORTO FUORI LE VARIABILI ALTRIMENTI CI SONO PROBLEMI CON LO SCOPE
-	int scelta, q_num, time_calc;
-
+	double *op, *op_loc;
 	double sum = 0.0, sum_parz = 0.0;
 
-	double t0 = 0.0, t1 = 0.0, time = 0.0;
-	double timetot = 0.0;
+	double t0 = 0.0, t1 = 0.0;
+	double time_loc = 0.0, time_tot = 0.0;
 
-	MPI_STATUS status;
-
-	/* ************************************************************************ */
-	// INTRODUZIONE
-
-	printf("Inizio esecuzione.\n\n");
+	MPI_Status status;
 
 	/* ************************************************************************ */
+	// INIZIALIZZAZIONE DI MPI
+
+
+	/*
+		Definisce l'insieme dei processori attivati (contesto),
+		assegnandone un identificativo
+	*/
+
+	MPI_Init(&argc, &argv);
+	
+	/*
+		Assegna ad ogni processore del communicator l'identificativo
+		'id_proc' (sempre associato al contesto).
+		MPI_COMM_WORLD indica il communicator a cui appartengono tutti
+		i processori attivati (non può essere alterato).
+	*/
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &id_proc);
+	
+	/*
+		Restituisce ad ogni processore di MPI_COMM_WORLD
+		il numero di processori nel contesto.
+	*/
+
+	MPI_Comm_size(MPI_COMM_WORLD, &n_proc);
+
+	/* ******************************************************************** */
 	// LETTURA E DISTRIBUZIONE DEI DATI
+	
+	if (id_proc == 0) {
+		printf("Inizio esecuzione.\n\n");
 
-	MPI_Init(&argv, &argc);
-	MPI_Comm_rank(MPI_COMM_WORLD, &menum);
-	MPI_Comm_size(MPI_COMM_WORLD, &nproc);
-
-	if (menum == 0) {
-		/* ******************************************************************** */
-		// LETTURA DEGLI ARGOMENTI
+		/*
+			Affidiamo al primo processore il compito di leggere
+			gli argomenti in input 'argv[]'.
+		*/
 
 		if (argc < 4) {
 			printf("Errore nella lettura degli argomenti di input!\n\n");
 			printf("Esecuzione terminata.\n");
+			MPI_Finalize();
 			exit(1);
 		}
 
@@ -114,57 +148,79 @@ int main(int argc, char const *argv[]) {
 		time_calc = argToInt(argv[3]);
 
 		// printf("scelta: %d, q_num: %d, time_calc: %d\n\n", scelta, q_num, time_calc);
+	}
 
-		double op[q_num];
+	/*
+		Si utilizza questa funzione per inviare gli argomenti letti dal
+		processore 0 (root) a tutti i processori del contesto, incluso se' stesso.
+		Cioè, i valori di 'scelta', 'q_num' e 'time_calc' (interi di dimensione 1)
+		saranno copiati in tutti i processori del communicator MPI_COMM_WORLD.
+	*/
 
-		for (int i=0; i < q_num; i++) {
+	MPI_Bcast(&scelta, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&q_num, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&time_calc, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	// Si distribuisce equamente il numero di operandi tra tutti i processori.
+	q_loc = q_num / n_proc;
+
+	//	Gli operandi rimanenti sono distribuiti tra i primi 'rest' processori.
+	rest = q_num % n_proc;
+	if (rest != 0  && id_proc < rest) {
+		q_loc++;
+	}
+
+	op = (double *)calloc (q_num, sizeof(double));
+	op_loc = (double *)calloc (q_loc, sizeof(double));
+
+	if (id_proc == 0) {
+		for (i=0; i < q_num; i++) {
 			op[i] = argToDouble(argv[i+4]);
 			// printf("--- op[i]: %f ---\n", op[i]);
 		}
 	}
 
-	MPI_Bcast(&q_num, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(op, q_num, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-	nloc = n / nproc;
-	rest = n % nproc;
-	if (menum < rest) {
-		nloc++;
-	}
+	if (id_proc == 0) {
+		op_loc = op;
+		tmp = q_loc;
+		id_op = 0;
 
-	// TODO: allocazione di xloc
-
-	if (menum == 0) {
-		xloc = x;
-		tmp = nloc;
-		for (int i=1; i < nproc; i++) {
-			start = start + tmp;
+		for (i=1; i < n_proc; i++) {
+			id_op = id_op + tmp;
 			tag = i +22;
 			if (i == rest) {
 				tmp--;
 			}
 
-			MPI_Send(&x[start], tmp, MPI_INT, i, tag, MPI_COMM_WORLD);
+			MPI_Send(&op[id_op], tmp, MPI_DOUBLE, i, tag, MPI_COMM_WORLD);
 		}
+
 	} else {
-		tag = menum +22;
-		MPI_Recv(xloc, nloc, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+		tag = id_proc +22;
+		MPI_Recv(op_loc, q_loc, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
 	}
 
 	/* ************************************************************************ */
 	// CALCOLO DELLA SOMMA PARZIALE
 
-	if (scelta != 4) {
+	// printf("\nProcesso n.%d\n", id_proc);
+	// printf("scelta: %d, q_num: %d, time_calc: %d\n", scelta, q_num, time_calc);
+	// printf("q_loc: %d, q_num: %d, time_calc: %d\n", scelta, q_num, time_calc);
+
+	if (id_proc == 0 && scelta != 4) {
 		printf("Applicazione della strategia %d.\n", scelta);
 	}
 
 	if (time_calc == 1) {
-		MPI_Barrier(MPI_COMM_WORLD);
+		// MPI_Barrier(MPI_COMM_WORLD);
 		t0 = MPI_Wtime();
 	}
 
 	sum = 0.0;
-	for(int i=0; i < nloc; i++) {
-		sum = sum + xloc[i];
+	for(i=0; i < q_loc; i++) {
+		sum = sum + op_loc[i];
 	}
 
 	/* ************************************************************************ */
@@ -173,48 +229,47 @@ int main(int argc, char const *argv[]) {
   	switch (scelta) {
 		case 1: // Applicazione della strategia 1.
 		{
-			if (menum == 0) {
-				for(int i=1; i < nproc; i++) {
+			if (id_proc == 0) {
+				for(i=1; i < n_proc; i++) {
 					tag = i +80;
-					MPI_Recv(&sum_parz, 1, MPI_INT, i, tag, MPI_COMM_WORLD, &status);
+					MPI_Recv(&sum_parz, 1, MPI_DOUBLE, i, tag, MPI_COMM_WORLD, &status);
 					sum = sum + sum_parz;
 				}
 			} else {
-				tag = menum +80;
-				MPI_Recv(&sum, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status); //ci mancava status
+				tag = id_proc +80;
+				MPI_Send(&sum, 1, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
 			}
 
 			break;
 		}
 		case 2: // Applicazione della strategia 2.
 		{
-			for(int i=0; i < log2(nproc); i++) { // passi di comunicazione
-				if ((menum % pow(2.0, i)) == 0) { // chi partecipa alla comunicazione -- pow() vuole i double non gli int
-					if ((menum % pow(2.0, i+1)) == 0) { // chi riceve le comunicazioni -- pow() vuole i double non gli int
-						// ricevi da menum+2^i
-					} else {
-						// spedisci a menum-2^i
-					}
-				}
-			}
-
-			printf("\nProcesso n.%d\n", menum);
-			printf("\nLa somma totale e' %f\n", sum);
+			// for(i=0; i < log(n_proc); i++) { // passi di comunicazione
+			// 	p = pow(2, i);
+			// 	if ((id_proc % p) == 0) { // chi partecipa alla comunicazione
+			// 		p = pow(2, i+1);
+			// 		if ((id_proc % p) == 0) { // chi riceve le comunicazioni
+			// 			// ricevi da id_proc+2^i
+			// 		} else {
+			// 			// spedisci a id_proc-2^i
+			// 		}
+			// 	}
+			// }
 
 			break;
 		}
 		case 3: // Applicazione della strategia 3.
 		{
-			for(int i=0; i < log2(nproc); i++) { // passi di comunicazione
-				// tutti i processi partecipano ad ogni passo
-				if ((menum % pow(2.0, i+1)) < pow(2.0, i)) { // si decide solo a chi si invia e da chi si riceve -- pow() vuole i double non gli int
-					// ricevi da menum+2^i
-					// spedisci a menum+2^i
-				} else {
-					// ricevi da menum-2^i
-					// spedisci a menum-2^i
-				}
-			}
+			// for(i=0; i < log2(n_proc); i++) { // passi di comunicazione
+			// 	// tutti i processi partecipano ad ogni passo
+			// 	if ((id_proc % pow(2, i+1)) < pow(2, i)) { // si decide solo a chi si invia e da chi si riceve
+			// 		// ricevi da id_proc+2^i
+			// 		// spedisci a id_proc+2^i
+			// 	} else {
+			// 		// ricevi da id_proc-2^i
+			// 		// spedisci a id_proc-2^i
+			// 	}
+			// }
 
 			break;
 		}
@@ -238,17 +293,17 @@ int main(int argc, char const *argv[]) {
 
 	if (time_calc == 1) {
 		t1 = MPI_Wtime();
-		time = t1 - t0;
-		MPI_Reduce(&time, &timetot, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-		if (menum == 0) {
-			printf("\nApplicazione della strategia %d terminata in %esec\n", scelta, timetot);
+		time_loc = t1 - t0;
+		MPI_Reduce(&time_loc, &time_tot, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+		if (id_proc == 0) {
+			printf("\nApplicazione della strategia %d terminata in %esec\n", scelta, time_tot);
 		}
 	}
 
 	switch (scelta) {
 		case 1:
 		{
-			if (menum == 0) {
+			if (id_proc == 0) {
 				printf("\nLa somma totale e' %f\n", sum);
 			}
 			break;
@@ -256,7 +311,7 @@ int main(int argc, char const *argv[]) {
 		case 2:
 		case 3:
 		{
-			printf("\nProcesso n.%d\n", menum);
+			printf("\nProcesso n.%d\n", id_proc);
 			printf("\nLa somma totale e' %f\n", sum);
 		}
 		case 4:
@@ -269,7 +324,16 @@ int main(int argc, char const *argv[]) {
 
   	/* ************************************************************************ */
 
-	printf("\nEsecuzione terminata.\n");
+	/*
+		Determina la fine del programma MPI.
+		Da questo punto in poi non è possibile richiamare altre funzioni MPI.
+	*/
+
+	MPI_Finalize();
+
+	if (id_proc == 0) {
+		printf("\nEsecuzione terminata.\n");
+	}
 	return 0;
 }
 
